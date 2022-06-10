f240272e6def640901ba2b32930babbedfd2666a
SagarDNG <sagardng123@gmail.com>
2022-06-10 14:23:57 +0530

    bug fix

--- app.js
+++ app.js
@@ -5,31 +5,18 @@ const year = document.getElementById('number');
 
 button.addEventListener('click', check);
 
-// function check() {
-//     if (year.value % 4 == 0) {
-//         if (year.value % 100 == 0) {
-//             if (year.value % 400 == 0) {
-//                 
-//             }
-//             else
-//                 result.innerHTML = `No, ${year.value} is not a Leap Year`;
-//         }
-//         else
-//             result.innerHTML = `Yes, ${year.value} is a Leap Year`;
-//     }
-//     else
-//         result.innerHTML = `No, ${year.value} is not a Leap Year`;
-
-//     
-
-// }
 function check() {
-    if (year.value % 4 === 0 || year.value % 400 === 0) {
-        result.innerHTML = `Yes, ${year.value} is a Leap Year`;
+    if (!year.value) {
+        alert("Please enter some year, Don't leave it blank")
     }
     else {
-        result.innerHTML = `No, ${year.value} is not a Leap Year`;
+        if (year.value % 4 === 0 || year.value % 400 === 0) {
+            result.innerHTML = `Yes, ${year.value} is a Leap Year`;
+        }
+        else {
+            result.innerHTML = `No, ${year.value} is not a Leap Year`;
+        }
+        checkOtherYear.innerHTML = "Now, Check a different Year!";
+        year.value = '';
     }
-    checkOtherYear.innerHTML = "Now, Check a different Year!";
-    year.value = '';
 }
